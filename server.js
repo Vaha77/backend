9a30552e35ba93d505fb6cc8ac9253857ffc6e65
vaha77 <abdulvohid996@gmail.com>
2022-06-28 16:52:31 +0900

    vaha

--- server.js
+++ server.js
@@ -2,9 +2,45 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
+app.use("/users", (req, res) => {
   res.statusCode = 500;
-  res.send("salom becend men keldim");
+  // res.send("salom becend men keldim");
+  res.json({
+    categoryId: 0,
+    city: "string",
+    componentsDto: {
+      additional: "string",
+      airCondition: true,
+      courtyard: true,
+      furniture: true,
+      gasStove: true,
+      internet: true,
+      tv: true,
+    },
+    country: "",
+    description: " ",
+    favorite: true,
+    homeAmenitiesDto: {
+      additional: "string",
+      busStop: true,
+      garden: true,
+      market: true,
+      park: true,
+      parking: true,
+      school: true,
+      stadium: true,
+      subway: true,
+      superMarket: true,
+    },
+    houseDetails: {
+      area: "",
+      bath: "",
+      beds: "",
+      garage: "",
+      room: "",
+      yearBuilt: "",
+    },
+  });
 });
 
 app.listen(5000, () => {
