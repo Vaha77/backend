c49ce4b12b53f4b8ac63ab904f33c4c7e68098de
vaha77 <abdulvohid996@gmail.com>
2022-07-01 10:43:19 +0900

    vaha

---
+++ list.js
@@ -0,0 +1,16 @@
+// import av from "./bk.jpg";
+
+const list = [
+  { id: 1, name: Vokhidjon, first: Rakhimov, img: "av" },
+  { id: 2, name: Jamshid, first: Hechlim, img: "as" },
+  { id: 3, name: hechkimboy, first: Jam, img: "aa" },
+  { id: 4, name: Kim, first: yuli, img: "ad" },
+  // { id: 5, name: Vokhidjon, first: Rakhimov, img: "av" },
+  // { id: 6, name: Vokhidjon, first: Rakhimov, img: "av" },
+  // { id: 7, name: Vokhidjon, first: Rakhimov, img: "av" },
+  // { id: 8, name: Vokhidjon, first: Rakhimov, img: "av" },
+  // { id: 9, name: Vokhidjon, first: Rakhimov, img: "av" },
+  // { id: 10, name: Vokhidjon, first: Rakhimov, img: "av" },
+];
+
+module.exports = list;
--- server.js
+++ server.js
@@ -5,12 +5,20 @@ const app = express();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  // res.send("salom");
-  res.json({
-    name: "Rakhimov",
-    title: "Vaxa",
-  });
+const list = require(`./list`);
+
+app.get("./", (req, res, next) => {
+  res.json(list);
 });
 
-app.listen(3002);
+// app.get("/", (req, res) => {
+// res.send("salom");
+// res.json({
+//   name: "Rakhimov",
+//   title: "Vaxa",
+// });
+// res.sendFile("./package.json", { root: __dirname });
+// res.download("./bk.jpg");
+// });
+
+app.listen(3002, () => console.log("server yurdi"));
